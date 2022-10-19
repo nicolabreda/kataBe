8cb16a469a7f1b9453fb6e024f496df32278b4a6
Alfonso Maiorino <a.maiorino@be-tse.it>
2022-10-19 15:01:33 +0200

    step 1

---
+++ codice/paoloAlfonso/src/main/java/it/paoloalfonso/kata/CodificaBinaria.java
@@ -0,0 +1,9 @@
+package it.paoloalfonso.kata;
+
+public class CodificaBinaria {
+
+    public static Long calculateDecimalFromBinary(String binaryNumber) {
+        return Long.parseLong(binaryNumber, 2);
+    }
+
+}
---
+++ codice/paoloAlfonso/src/main/java/it/paoloalfonso/kata/MainClass.java
@@ -0,0 +1,12 @@
+package it.paoloalfonso.kata;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Numero 11: " + CodificaBinaria.calculateDecimalFromBinary("11"));
+        System.out.println("Numero 100101100011: " + CodificaBinaria.calculateDecimalFromBinary("100101100011"));
+        System.out.println("Numero 100101001001001100011: " + CodificaBinaria.calculateDecimalFromBinary("100101001001001100011"));
+        System.out.println("Numero 100101001001001010010010001000001000100010100011: " + CodificaBinaria.calculateDecimalFromBinary("100101001001001010010010001000001000100010100011"));
+    }
+
+}
