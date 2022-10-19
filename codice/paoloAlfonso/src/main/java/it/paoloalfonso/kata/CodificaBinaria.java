969cd60889449e7d08603e6def731d763dd0105d
Alfonso Maiorino <a.maiorino@be-tse.it>
2022-10-19 16:55:32 +0200

    step 2

--- codice/paoloAlfonso/src/main/java/it/paoloalfonso/kata/CodificaBinaria.java
+++ codice/paoloAlfonso/src/main/java/it/paoloalfonso/kata/CodificaBinaria.java
@@ -1,9 +1,63 @@
 package it.paoloalfonso.kata;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CodificaBinaria {
+
+    private static Map<Character, String> hexMap = new HashMap<>();
+
+    static {
+        hexMap.put('0', "0000");
+        hexMap.put('1', "0001");
+        hexMap.put('2', "0010");
+        hexMap.put('3', "0011");
+        hexMap.put('4', "0100");
+        hexMap.put('5', "0101");
+        hexMap.put('6', "0110");
+        hexMap.put('7', "0111");
+        hexMap.put('8', "1000");
+        hexMap.put('9', "1001");
+        hexMap.put('A', "1010");
+        hexMap.put('B', "1011");
+        hexMap.put('C', "1100");
+        hexMap.put('D', "1101");
+        hexMap.put('E', "1110");
+        hexMap.put('F', "1111");
+    }
 
     public static Long calculateDecimalFromBinary(String binaryNumber) {
         return Long.parseLong(binaryNumber, 2);
+    }
+
+    public static String convertHexToBinary(String hexString) {
+        char[] hexChars = hexString.toCharArray();
+        String binString = "";
+        for(char hexChar : hexChars) {
+            binString += hexMap.get(hexChar);
+        }
+        return binString;
+    }
+
+    public static Pacchetto convertBinaryString(String binaryString) {
+        String version = binaryString.substring(0, 3);
+        String type = binaryString.substring(3, 6);
+        String message = binaryString.substring(6);
+        return new Pacchetto(calculateDecimalFromBinary(version), calculateDecimalFromBinary(type), message);
+    }
+
+    public static Long convertMessageToBinary(String message) {
+        Integer numberOfBlocks = message.length() / 5;
+        String converted = "";
+        if(message.length() % 5 > 0) numberOfBlocks++;
+        for(int i = 0; message.length() % 5 > 0 ? i < numberOfBlocks - 1 : i < numberOfBlocks; i++) {
+            String block = message.substring(i * 5, (i + 1) * 5);
+            boolean hasNext = block.substring(0, 1).equals("1");
+            converted += block.substring(1, 5);
+            if(!hasNext) break;
+        }
+        return Long.parseLong(converted,2);
     }
 
 }
--- codice/paoloAlfonso/src/main/java/it/paoloalfonso/kata/MainClass.java
+++ codice/paoloAlfonso/src/main/java/it/paoloalfonso/kata/MainClass.java
@@ -1,12 +1,20 @@
 package it.paoloalfonso.kata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainClass {
 
+    private static List<String> values = Arrays.asList("304", "9227", "D25130", "F31FE66F790C", "52D084A5BA6E69", "725C8E3F8BCF319FB68");
+
     public static void main(String[] args) {
-        System.out.println("Numero 11: " + CodificaBinaria.calculateDecimalFromBinary("11"));
-        System.out.println("Numero 100101100011: " + CodificaBinaria.calculateDecimalFromBinary("100101100011"));
-        System.out.println("Numero 100101001001001100011: " + CodificaBinaria.calculateDecimalFromBinary("100101001001001100011"));
-        System.out.println("Numero 100101001001001010010010001000001000100010100011: " + CodificaBinaria.calculateDecimalFromBinary("100101001001001010010010001000001000100010100011"));
+        values.stream().forEach(v -> {
+            String binary = CodificaBinaria.convertHexToBinary(v);
+            Pacchetto pacchetto = CodificaBinaria.convertBinaryString(binary);
+            Long numero = CodificaBinaria.convertMessageToBinary(pacchetto.getMessage());
+            System.out.println(String.format("Binary %s;\n Pacchetto: %s\n, numero: %s", binary, pacchetto, numero));
+        });
     }
 
 }
