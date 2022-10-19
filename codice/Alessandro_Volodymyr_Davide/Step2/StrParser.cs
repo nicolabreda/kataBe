41dce911a2d4431f0926da92db17ff361a6677dc
Yarynych Volodymyr <v.yarynych.ita@be-tse.it>
2022-10-19 15:50:34 +0200

    step 2 soluzione

---
+++ codice/Alessandro_Volodymyr_Davide/Step2/StrParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step1_CodeKata
+{
+    class StrParser
+    {
+
+        public int ParseBinaryLine(string binary)
+        {
+            
+            int tipologia = Convert.ToInt32(binary.Substring(3, 3), 2);
+            switch (tipologia)
+            {
+                case 4:
+                    TipoStringa tipoStr = new TipoStringa();
+                    return tipoStr.ParseTipo4(binary.Substring(6));
+                    break;
+                default:
+                    return -1;
+
+            }
+
+        }
+    }
+}
---
+++ codice/Alessandro_Volodymyr_Davide/Step2/TipoStringa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step1_CodeKata
+{
+    class TipoStringa
+    {
+        public int ParseTipo4(string hex)
+        {
+            int result = 0;
+            int position = 0;
+            while (true)
+            {
+                
+                string subStr = hex.Substring(position, 5);
+                result += Convert.ToInt32(subStr.Substring(1), 2);
+                if (subStr[0] == '0')
+                {
+                    break;
+                }
+                position += 5;
+            }
+            return result;
+        }
+
+    }
+}
